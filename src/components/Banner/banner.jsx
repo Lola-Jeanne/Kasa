7c1e084e5684cc741af92cbc4f682e55423538a2
CARLA LOPEZ <carlalolajeanne@gmail.com>
2024-09-03 19:25:42 +0200

    :construction: mise en page banner home et about

--- src/components/Banner/banner.jsx
+++ src/components/Banner/banner.jsx
@@ -1,8 +1,8 @@
-import stylesBanner from "./banner.styles.scss";
+import "./banner.styles.scss";
 
 
 export default function Banner({title, urlImage}){
-    return<div className={stylesBanner}>
+    return<div className="containerBanner">
         <div className="banner">
             <img src={urlImage} className="imgBanner"></img>
             <p className="txtBanner">{title}</p>
--- src/pages/About/about.jsx
+++ src/pages/About/about.jsx
@@ -1,6 +1,8 @@
 import Header from "../../components/Header/header";
 import Footer from "../../components/Footer/footer";
 import Dropdowns from "../../components/Dropdowns/dropdown";
+import Banner from "../../components/Banner/banner";
+import bannerPicTwo from "/Users/carlalopez/Documents/Openclassrooms/Projet 8/Workflow/kasa/src/assets/Banner-img-two.png";
 
 function About () {
     return (
@@ -8,6 +10,9 @@ function About () {
             <Header />
             <div>
                 <Footer />
+                <div>
+                    <Banner urlImage={bannerPicTwo}/>
+                </div>
                 <div>
                     <Dropdowns /> 
                 </div>
