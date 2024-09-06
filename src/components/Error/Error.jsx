c158b6bf8ef55746fddb3445ca9530f707a2dabb
CARLA LOPEZ <carlalolajeanne@gmail.com>
2024-09-06 16:25:41 +0200

    :iphone: responsive page home

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -7,7 +7,7 @@ function Error () {
         <div className={styles.error}>
             <Header />
             <h1>404</h1>
-            <p>Oups! La page que vous demandez n'existe pas.</p>
+            <p className="txtError">Oups! La page que vous demandez n'existe pas.</p>
             <a className="btnRetour" href="/">Retourner sur la page d'accueil</a>
             <div>
                 <Footer />
--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,15 +1,17 @@
 import { NavLink } from "react-router-dom";
-import styles from "./header.styles.scss";
+import "./header.styles.scss";
 import logo from "../../assets/LOGO.svg";
 
 export default function Header(){
-    return <header className={styles.header}>
+    return <header>
                 <img src={logo} alt="Logo du site Kasa"></img>
                 <div>
-                    <NavLink to="/" className={({ isActive, NavLink }) => isActive ? styles.activeLink : undefined}>
+                    <NavLink to="/" className={({ isActive }) => isActive ? 'activeLink' : ''}
+                    end
+                    >
                     Accueil
                     </NavLink>
-                    <NavLink to="/about" className={({ isActive, NavLink}) => isActive ? styles.activeLink : undefined}>
+                    <NavLink to="/about" className={({ isActive }) => isActive ? 'activeLink' : ''}>
                     A Propos
                     </NavLink>
                 </div>
